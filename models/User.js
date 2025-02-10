64a23cd895b79878b80ebfaf240b6c427c7290ff
SasiPriya-25 <priyangakaliyappann@gmail.com>
2025-02-10 13:45:47 +0530

    Changes in User model

--- models/User.js
+++ models/User.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  name: String,
+  name: { type: String, required: true },
   email: { type: String, unique: true },
-  password: String
+  age: { type: Number, required: true }
 });
 
 module.exports = mongoose.model('User', userSchema);
